39c02fe87c71a75c854ea5943204d07fb83db491
GamingDevilsCC <gamingdevilscc@gmail.com>
2020-01-07 22:11:47 -0500

    Development 6: Streamline and add more menus
    Changelog:
    Streamlined blocks of redundant code in the recycler function.
    Added WIP main menu and scoreboard menu.
    
    Signed-off-by: GamingDevilsCC <gamingdevilscc@gmail.com>

--- Recyclers vs. E-Waste/game.py
+++ Recyclers vs. E-Waste/game.py
@@ -10,26 +10,46 @@ from pygame import*
 import os
 import random
 
-#this is under development. Once done, the user is able to see his score and other player's score, and add his score to the scoreboard.
+#this is under development, but is only visible once the user dies. Once done, the user is able to see his score and other player's score, and add his score to the scoreboard.
 def scoreboardMenu():
     global scoreboardList
     numFile = open("scoreboard.dat", "r")
-    while True:
-        text = numFile.readline()
-        #rstrip removes the newline character read at the end of the line
-        text = text.rstrip("\n")
-        if text=="": 
-            break
-        text = text.split(",")
-        scoreboardList.append(text)
-    numFile.close()    
-    draw.rect(screen, BLACK, (0, 0, 1000, 700))
-    
+    if len(scoreboardList) == 0: #this way, when the game keeps calling this function, it doesn't repeat the same data into the scoreboardList.
+        while True:
+            text = numFile.readline()
+            #rstrip removes the newline character read at the end of the line
+            text = text.rstrip("\n")
+            if text=="": 
+                break
+            text = text.split(",")
+            scoreboardList.append(text)
+        numFile.close()    
+        draw.rect(screen, BLACK, (0, 0, 1000, 700))
+        for record in range(len(scoreboardList)):
+            for field in range(len(scoreboardList[record])):
+                displayText = smallFont.render(scoreboardList[record][field], 1, WHITE)
+                screen.blit(displayText, Rect(field * 50, record * 30, 500, 500))    
+        
+def mainMenu(): #main menu
+    global mx, my
+    global menu
+    global game
+    draw.rect(screen, BLACK, (0, 0, 100, 700))
+    draw.rect(screen, BLUE, (450, 375, 100, 50))
+    if 550 > mx > 450 and 425 > my > 375:
+        game = True
+        menu = False
 
 def gameMenu(): #game
     global roundStarted
     global generateEnemies
     global greenCoins
+    global game
+    global scoreboard
+    
+    if playerHealth <= 0:
+        game = False
+        scoreboard = True
     #track
     draw.rect(screen, BLACK, (0, 0, 1000, 700))
     draw.rect(screen, WHITE, (0, 200, 1000, 100)) #top track
@@ -69,9 +89,7 @@ def gameMenu(): #game
         enemiesMoving()
     recycler()
     
-    #time.wait(5)
-    display.flip()
-    userEvents()    
+    #userEvents()
     
 #animation for the e-waste moving through the screen.
 def enemiesMoving(): #will have feature to randomly select different levels of enemies soon.
@@ -87,6 +105,11 @@ def enemiesMoving(): #will have feature to randomly select different levels of e
             enemyHealthText = smallFont.render(str(eWasteHealth[eWaste]), 1, WHITE)
             screen.blit(enemyHealthText, Rect(eWasteX[eWaste] + 5, eWasteY[eWaste] + 5, 0, 0)) #e-waste health text
             eWasteX[eWaste] += eWasteSpeed[eWaste] #increase the x position of e-waste to the right by it's speed.
+
+        #code saying if the enemy reaches the end of the path, reduce player health
+        if eWasteX[eWaste] == 990: #if the e-waste the end of the path.
+            playerHealth -= 10
+            eWasteStatus[eWaste] = 0 #0 for dead, 1 for alive        
     
     #Damage protocol, used for determining colision and ewaste/player health. If ewaste reaches the end or is out of health, it gets removed from the  
     for machine in range(len(recyclerX)):
@@ -97,12 +120,6 @@ def enemiesMoving(): #will have feature to randomly select different levels of e
                     if eWasteHealth[eWaste] <= 0: #if the health of the e-waste is dead, delete it from list
                         eWasteStatus[eWaste] = 0 #it's not alive, so change it's status to dead
                         greenCoins += eWasteCoinDrop[eWaste] #give the coins that it dropped
-                        
-                #code saying if the enemy reaches the end of the path, reduce player health
-                elif eWasteX[eWaste] == 990: #if the e-waste the end of the path.
-                    playerHealth -= 10
-                    eWasteStatus[eWaste] = 0 #0 for dead, 1 for alive
-                    #maybe simplify code above by changing health to 0 when it contacts edge?
 
 def enemyTier1(roundNumber):
     global eWasteX
@@ -165,48 +182,35 @@ def recycler():
     if 550 > mx > 450 and 650 > my > 600 and greenCoins >= 10:
         placeRecycler = True
     
-    xPosition = recyclerPositionFactor(mx)
-    
-    #places it in the lane the user wants it to be in a grid with 90 x 90 squares.
-    if placeRecycler == True and 400 > my > 300: #user placing recycler on middle track
-        xPosition = recyclerPositionFactor(mx)
-        if recyclerPlacementCheck(xPosition, 325) == False:
-            recyclerX.append(xPosition)
-            recyclerY.append(325)
-            recyclerDamage.append(50) #this is level 1 damage that recycler does to e-waste
-            mx = 0
-            my = 0
-            placeRecycler = False
-            greenCoins -= 10
-        else: print("error: already placed, click another spot")
-    
-    elif placeRecycler == True and 300 > my > 200: #user placing recycler on top track
-        xPosition = recyclerPositionFactor(mx)
-        if recyclerPlacementCheck(xPosition, 225) == False:
-            recyclerX.append(xPosition)
-            recyclerY.append(225)
-            recyclerDamage.append(50) #this is level 1 damage that recycler does to e-waste
-            mx = 0
-            my = 0
-            placeRecycler = False
-            greenCoins -=10
-        else: print("error: already placed, click another spot")
-    
-    elif placeRecycler == True and 500 > my > 400: #user placing recycler on bottom track
-        xPosition = recyclerPositionFactor(mx)
-        if recyclerPlacementCheck(xPosition, 425) == False:
-            recyclerX.append(xPosition)
-            recyclerY.append(425)
-            recyclerDamage.append(50) #this is level 1 damage that recycler does to e-waste
-            mx = 0
-            my = 0
-            placeRecycler = False
-            greenCoins -=10
-        else: print("error: already placed, click another spot")
+    xPosition = recyclerPositionFactor(mx) #this makes the placement of the recyclers follow a grid that's spaced by 90 pixels.
+    if placeRecycler == True:
+        #place it in the lane the user wants it to be in, following the invisible grid.
+        if 400 > my > 300: #user placing recycler on middle track
+            placingRecycler(xPosition, 325)
+        
+        elif 300 > my > 200: #user placing recycler on top track
+            placingRecycler(xPosition, 225)
+        
+        elif 500 > my > 400: #user placing recycler on bottom track
+            placingRecycler(xPosition, 425)
     
     #draw the recycler
     for machine in range(len(recyclerX)): #how many recyclers there are.                    
         draw.rect(screen, GREEN, (recyclerX[machine], recyclerY[machine], 50, 50)) #screen, color, (x, y, w, l)
+
+#this function places the recyclers onto the map if there isn't one already placed in the desired location
+def placingRecycler(xPosition, yLane):
+    global placeRecycler
+    global greenCoins
+    if recyclerPlacementCheck(xPosition, yLane) == False:
+        recyclerX.append(xPosition)
+        recyclerY.append(yLane)
+        recyclerDamage.append(50) #this is level 1 damage that recycler does to e-waste
+        mx = 0
+        my = 0
+        placeRecycler = False
+        greenCoins -= 10
+    else: print("error: already placed, click another spot")
 
 #This function checks if there is already a recycler where the user is trying to place it.
 def recyclerPlacementCheck(mx, my):
@@ -243,7 +247,6 @@ def startButton():
     #... another wave of enemies before the current one has finished.
     if onGoing == False:
         roundStarted = False
-        
         
     if (50 > mx > 0 and 20 > my > 0) and roundStarted == False: #check if the button is clicked and if the round has not already started
         roundStarted = True
@@ -284,7 +287,9 @@ myClock = time.Clock()
 smallFont = font.SysFont("Arial", 18)
 
 running = True
-game = True #we haven't created a main menu yet, so set this to true for now.
+game = False #we haven't created a main menu yet, so set this to true for now.
+menu = True
+scoreboard = False
 started = True
 placeRecycler = False #true for now, used for placing recycler
 roundStarted = False #if a round / wave / match is started.
@@ -295,7 +300,7 @@ roundNumber = 0
 mx = 0
 my = 0
 
-playerHealth = 100
+playerHealth = 10
 greenCoins = 50 #this is the currency / coin system
 score = 0
 
@@ -312,10 +317,18 @@ eWasteStatus = []
 eWasteCoinDrop = []
 eWasteSpeed = []
 
+scoreboardList = []
+
 #entire loop
 while running:
-    if game == True:
+    if menu == True:
+        mainMenu()
+    elif game == True:
         gameMenu()
-
+    elif scoreboard == True:
+        scoreboardMenu()
+    
+    userEvents()
     myClock.tick(60)
+    display.flip()
 quit()
